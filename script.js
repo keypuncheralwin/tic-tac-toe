f1c8d44904e60929b0978e47d528c86d64f0b597
Alwin George <alwingeorge11@gmail.com>
2021-08-01 19:55:45 +0930

    laid the groundwork on a duplicate box checking system

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const boxes = document.querySelectorAll(".box")
 
 
-let player = 0
+let player = 0; //0 means player 1 and  1 means player 2
 
-let player1 = [];
-let player2 = [];
+let player1 = []; // player 1 position array
+let player2 = []; // player 2 position array
 
+let gameState = 0; //0 means game is ongoing, 1 means game is over
 
-hover(player)
+hover(player) //hover status, starts off red
 
 
 for (let box of boxes) {
@@ -17,37 +18,39 @@ for (let box of boxes) {
         if (player === 0) {
             const name = "X"
             let positionX = parseInt(box.getAttribute('data-pos'))
+            checkBox(positionX)
             player1.push(positionX)
             console.log(player1)
             box.textContent = name;
+            player = 1
             if (player1.length >= 3) {
-                check(name, player1);
+                checkWin(name, player1);
             }
             
-
-            player = 1
+            console.log(player)
+            
             hover(player)
 
         } else if (player === 1) {
             const name = "O"
             let positionO = parseInt(box.getAttribute('data-pos'))
+            checkBox(positionO)
             player2.push(positionO)
             console.log(player2)
             box.textContent = name;
-            if (player1.length >= 3) {
-                check(name, player2);
-            }
-
-            
             player = 0
+            if (player1.length >= 3) {
+                checkWin(name, player2);
+            }
+            
+            
+            
             hover(player)
-        }
+        }else alert('game over')
 
     })
     
 }
-
-
 
 const winCombos = [
     [0, 1, 2],
@@ -60,10 +63,14 @@ const winCombos = [
     [2, 4, 6]
 ]
 
-function check(name, array) {
+function checkWin(name, array) {
     for (win of winCombos) {
         if (win.every((val) => array.includes(val))) {
-            console.log(name + " is the winner")
+
+            alert(name + " is the winner")
+            player = 3
+            return true
+            
         }
 
     }
@@ -96,3 +103,12 @@ function hover(player) {
         
     }
 }
+
+
+function checkBox(position){
+    if(player1.includes(position) || player2.includes(position)){
+        alert("you can't click that box")
+    
+    }
+}
+
